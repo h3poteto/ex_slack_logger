52797a8989b45e74eb771748f5157389bfae1687
AkiraFukushima <h3.poteto@gmail.com>
2019-10-10 14:10:14 +0900

    Update package description

--- mix.exs
+++ mix.exs
@@ -5,9 +5,15 @@ defmodule SlackLogger.MixProject do
     [
       app: :slack_logger,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Logger backend module for Slack.",
+      package: [
+        maintainers: ["h3poteto"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/h3poteto/ex_slack_logger"}
+      ]
     ]
   end
 
@@ -26,8 +32,6 @@ defmodule SlackLogger.MixProject do
       {:timex, "~> 3.5"},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
